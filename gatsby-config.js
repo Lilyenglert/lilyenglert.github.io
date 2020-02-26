10d377307847f5b9fcc45a2c433e14d4639ce900
Lily Croskey-Englert <lmc327@cornell.edu>
2020-02-26 13:11:14 -0500

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lily Croskey-Englert`,
+    description: `Hi! I’m a product designer and frontend developer based in New York.`,
+    author: `Lily Croskey-Englert`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
