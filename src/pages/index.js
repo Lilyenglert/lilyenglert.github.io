0c7882fbd2f57de49379dc654c6dc5fc4de6ae96
lmc327 <lmc327@cornell.edu>
2020-02-25 23:30:47 -0500

    icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import ProjectBlock from "../components/project-block"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Lily Croskey Englert" />
     {/* <ProjectBlock blockTitle={"Lily Croskey Englert"} blockText ={"Hi! I’m a product designer and frontend developer based in New York. In December 2019 I graduated from Cornell University with a masters in Information Science. Before that, I was a design intern at IBM Watson in Boston bulding a design system for Watson Health."} blockVarient = {"simple-left"} >
     </ProjectBlock>
     <br></br>
@@ -26,7 +26,7 @@ const IndexPage = () => (
     <br></br>
     <br></br> */}
     <div className = 'center'>
-    <p className = 'placeholder-text'> <a>Hey, thanks for stopping by!</a> I'm currently updating my site and would love for you to see it when it's done. If you'd like a pdf version of my portfolio, shoot me an email at <a>lmc327@cornell.edu</a> — Lily</p>
+    <p className = 'placeholder-text'> <a>Hey, thanks for stopping by!</a> I'm currently updating my site and would love for you to see it when it's done. If you'd like a pdf version of my portfolio, shoot me an email at <a>lmc327@cornell.edu </a> — Lily</p>
     </div>
   </Layout>
 )
